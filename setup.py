84195c8e987b35363769338ef1dc16eee259ef27
QuadroLT <v.tamosiunas@gmail.com>
2018-11-11 14:57:45 +0200

    Added fake url

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ setup(name='regTools',
       version='0.1 alfa',
       description='''Package intended for different analytical chemistry tasks
       such as calibration, limit estimation, calculation of precision etc. see docs for details''',
-      url='',
+      url='my-git-repository.com',
       author='Vytautas TAMOSIUNAS',
       author_email='v.tamosiunas@gmail.com',
       license='BSD-3 clause',
       packages=['regTools'],
       zip_safe=False,
 )
-
