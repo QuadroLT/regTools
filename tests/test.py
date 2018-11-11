1d4d03cf94ff5900f33d82fed53ee0062221eb05
QuadroLT <v.tamosiunas@gmail.com>
2018-11-11 19:34:47 +0200

    Fixed tests to reflect current code.

--- tests/test.py
+++ tests/test.py
@@ -1,24 +1,28 @@
+import os
 import unittest
 import pandas as pd
-from regTools import Regression
+# from regTools import Regression
+from regTools.regression import *
 
 
 class RegModelTestCase(unittest.TestCase):
 
     def setUp(self):
-        data = pd.read_csv('11843.csv', sep=';')
+        os.chdir('..')
+        data = pd.read_csv('./datasets/11843-2ex2.csv', sep=';')
         self.inst = Regression(data, 'CONC', 'SIG')
+        os.chdir('tests')
 
-    def test_is_homoscedastic(self):
-        self.assertEqual(self.inst.is_homoscedastic(), False)
+    def test__levene_test(self):
+        self.assertLess(self.inst._levene_test(), 0.05)
 
-    def test_get_weights(self):
-        c, d = self.inst.get_weights()
+    def test__get_weights(self):
+        c, d = self.inst._get_weights()
         self.assertEqual('{:.2f}'.format(c), '4.46')
         self.assertEqual('{:.4f}'.format(d), '0.1501')
 
-    def test_fit(self):
-        a, b = self.inst.reg_model.params
+    def test__fit(self):
+        a, b = self.inst.model.params
         self.assertEqual('{:.3f}'.format(a), '12.218')
         self.assertEqual('{:.5f}'.format(b), '1.52727')
 
